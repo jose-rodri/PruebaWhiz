4fbe3b6c099915b5eae2fc4a19666af02de76a45
José Rodriguez <xt6223@rimac.com.pe>
2022-10-14 20:43:34 -0500

    Initial Commit

---
+++ PruebaWHIZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PruebaWHIZ
+//
+//  Created by Quispe Rodriguez, Jose Leoncio on 14/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
